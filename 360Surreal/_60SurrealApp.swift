253f6bd0cd493984ddb33855410292930107061f
wpl <william@360surreal.com>
2025-10-11 10:44:39 -0400

    Initial Commit

---
+++ 360Surreal/_60SurrealApp.swift
@@ -0,0 +1,17 @@
+//
+//  _60SurrealApp.swift
+//  360Surreal
+//
+//  Created by William Loftus on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct _60SurrealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
